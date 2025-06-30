d7518c15be9a6ffa771b07d2fb9e3c47263c89b4
THAY1688 <koemthay.tha@student.passerellesnumeriques.com>
2025-06-30 09:08:29 +0700

    my-portfolio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// js/main.js
+const { createApp, ref } = Vue;
+
+createApp({
+    setup() {
+        return {
+            // All data is now hardcoded in the HTML for better SEO and performance
+        }
+    }
+}).mount('#app');
